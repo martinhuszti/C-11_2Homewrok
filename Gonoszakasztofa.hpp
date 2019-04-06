cb94cdb327f476881d7416a4407378a9dafc220a
Martin <martinhuszti@gmail.com>
2019-04-06 16:47:04 +0200

    init

--- Gonoszakasztofa.hpp
+++ Gonoszakasztofa.hpp
@@ -2,20 +2,41 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include "Console_Write.hpp"
 
 class Gonoszakasztofa
 {
 private:
-  std::vector<std::string> words;
+std::vector<std::string> words;
 
 public:
-  Gonoszakasztofa(std::string &filename)
-  {
-    std::ifstream infile("thefile.txt");
+Gonoszakasztofa(std::string const &filename)
+{
+        std::ifstream infile("szavak.txt");
 
-    std::string word while (infile >> word)
-    {
-      words.push_back(word);
-    }
-  }
+        std::string word;
+        while (infile >> word)
+        {
+                words.push_back(word);
+        }
+}
+
+char RequestCharacter(){
+        AskForCharacter();
+        char character;
+        std::cin >> character;
+        return character
+}
+
+void SearchForMatch(){
+    
+}
+
+void play(){
+        Welcome();
+        std::cout << words[0];
+        char c = RequestCharacter();
+        SearchForMatch(c);
+}
+
 };
